038488dcd4eaa73fa9f4da09731f0cc959c35639
SakshamGupta31 <88344918+SakshamGupta31@users.noreply.github.com>
2022-07-29 19:09:16 +0530

    Add files via upload

---
+++ Portfolio/script.js
@@ -0,0 +1,7 @@
+console.log("script running...");
+document.querySelector('.close').style.display= 'none';
+document.querySelector('.menu').addEventListener("click",()=>{
+document.querySelector('.sidebar').classList.toggle('sidebarGo');
+   
+
+})
